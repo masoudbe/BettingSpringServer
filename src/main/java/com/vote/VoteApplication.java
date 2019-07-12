b421975bc5021822658a780e4b30870810920def
2008amin <52125684+2008amin@users.noreply.github.com>
2019-07-12 11:26:24 +0430

    amin
    amin

--- src/main/java/com/vote/VoteApplication.java
+++ src/main/java/com/vote/VoteApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class VoteApplication {
 
     public static void main(String[] args) {
+        //TestFromBrowser
         SpringApplication.run(VoteApplication.class, args);
     }
 }
